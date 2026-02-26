d0df82924c95db4a6452713075df24df936e8571
Akash077-spec <ak1406613@gmail.com>
2026-02-26 12:13:43 +0530

    add practice2.cpp

---
+++ examplereferecne.cpp
@@ -0,0 +1,2 @@
+// Practice1.cpp
+// 1-->  Example to Calculate are of rectangle.
---
+++ practice.cpp
@@ -0,0 +1,16 @@
+// this is a example to calculate the area of rectangle.
+#include<iostream>
+using namespace std;
+int main(){
+    int length, breadth;
+    cout<<"calculate area of rectangle"<<endl;
+    cout<<"enter the value of length"<<endl;
+    cin>>length;
+    cout<<"enter the value of breadth"<<endl;
+    cin>>breadth;
+    int area = length * breadth;
+    cout<<"The area of rectanlge is:"<<area<<endl;
+
+
+    return 0;
+}
